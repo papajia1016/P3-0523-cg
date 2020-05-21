a86747063a20bdf534725b72d63089a6d73c8fbd
XSSS12680 <65147908+XSSS12680@users.noreply.github.com>
2020-05-21 20:22:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-#自定插件
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
-git clone https://github.com/apollo-ng/luci-theme-darkmatter.git package/luci-theme-darkmatter
-git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 
