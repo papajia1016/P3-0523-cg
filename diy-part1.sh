a4e60d08e323d1a1bd5aa086ce77d2c9da505460
XSSS12680 <65147908+XSSS12680@users.noreply.github.com>
2020-05-27 17:18:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,20 +20,5 @@ sed -i 's/192.168.1.1/192.168.50.111/g' package/base-files/files/bin/config_gene
 
 #Add a feed source
 #sed -i '$a src-git lienol https://github.com/lienol/openwrt' feeds.conf.default
-cd openwrt
-rm -Rf package/lean/luci-theme-argon
 
-#自定插件
-git clone https://github.com/jerrykuku/luci-theme-argon -b 19.07_stable package/luci-theme-argon
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
-git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
-git clone https://github.com/apollo-ng/luci-theme-darkmatter.git package/luci-theme-darkmatter
-git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
-git clone https://github.com/rosywrt/luci-theme-rosy.git ./package/luci-theme-rosy
-
-#更新feeds
-./scripts/feeds update -a
-./scripts/feeds install -a
 
